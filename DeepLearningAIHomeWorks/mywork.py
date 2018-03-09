8cac6940f5acc909ca2d184cd799afb72ccff6ba
palanceli <palanceli@163.com>
2018-03-09 10:42:06 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -107,9 +107,161 @@ class Coding1_1(CodingWorks):
         value:[1 1 1 ... 1 1 0]
         '''
 
+    def sigmoid(self, z):
+        '''
+        4.1 - Helper functions
+        '''
+        s = 1 / (1 + np.exp(-z))
+        return s
+
+    '''
+    4.2 - Initializing parameters
+    '''
+    def initialize_with_zeros(self, dim):
+        ### START CODE HERE ### (≈ 1 line of code)
+        w = np.zeros((dim,1))
+        b = 0
+        ### END CODE HERE ###
+
+        assert(w.shape == (dim, 1))
+        assert(isinstance(b, float) or isinstance(b, int))
+        
+        return w, b
+
+    '''
+    4.3 - Forward and Backward propagation
+    '''
+    def propagate(self, w, b, X, Y):
+        m = X.shape[1]
+        
+        # FORWARD PROPAGATION (FROM X TO COST)
+        ### START CODE HERE ### (≈ 2 lines of code)
+        A = self.sigmoid(np.dot(w.T,X) + b)            # compute activation
+        cost = -(np.sum(np.dot(Y,np.log(A).T)+np.dot((1-Y),np.log(1-A).T)))/m      # compute cost
+        ### END CODE HERE ###
+        
+        # BACKWARD PROPAGATION (TO FIND GRAD)
+        ### START CODE HERE ### (≈ 2 lines of code)
+        dw = (np.dot(X,(A-Y).T))/m
+        db = (np.sum(A-Y))/m
+        ### END CODE HERE ###
+
+        assert(dw.shape == w.shape)
+        assert(db.dtype == float)
+        cost = np.squeeze(cost)
+        assert(cost.shape == ())
+        
+        grads = {"dw": dw,
+                 "db": db}
+        
+        return grads, cost
+
+    '''
+    4.4 - Optimization
+    '''
+    def optimize(self, w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+        costs = []
+        
+        for i in range(num_iterations):
+            # Cost and gradient calculation (≈ 1-4 lines of code)
+            ### START CODE HERE ### 
+            grads, cost = self.propagate(w, b, X, Y)
+            ### END CODE HERE ###
+            
+            # Retrieve derivatives from grads
+            dw = grads["dw"]
+            db = grads["db"]
+            
+            # update rule (≈ 2 lines of code)
+            ### START CODE HERE ###
+            w = w - learning_rate * dw
+            b = b - learning_rate * db
+            ### END CODE HERE ###
+            
+            # Record the costs
+            if i % 100 == 0:
+                costs.append(cost)
+            
+            # Print the cost every 100 training examples
+            if print_cost and i % 100 == 0:
+                print ("Cost after iteration %i: %f" %(i, cost))
+        
+        params = {"w": w,
+                  "b": b}
+        
+        grads = {"dw": dw,
+                 "db": db}
+        
+        return params, grads, costs
+
+        '''
+        '''
+    def predict(self, w, b, X):
+        m = X.shape[1]
+        Y_prediction = np.zeros((1,m))
+        w = w.reshape(X.shape[0], 1)
+        
+        # Compute vector "A" predicting the probabilities of a cat being present in the picture
+        ### START CODE HERE ### (≈ 1 line of code)
+        A = self.sigmoid(np.dot(w.T,X) + b)
+        ### END CODE HERE ###
+
+        for i in range(A.shape[1]):
+            
+            # Convert probabilities A[0,i] to actual predictions p[0,i]
+            ### START CODE HERE ### (≈ 4 lines of code)
+            if A[:,i] >= 0.5:
+                Y_prediction[:, i] = 1
+            else:
+                Y_prediction[:, i] = 0
+            ### END CODE HERE ###
+        
+        assert(Y_prediction.shape == (1, m))
+        
+        return Y_prediction
+
+    '''
+    5 - Merge all functions into a model
+    '''
+    def model(self, X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+        ### START CODE HERE ###
+        
+        # initialize parameters with zeros (≈ 1 line of code)
+        w, b = self.initialize_with_zeros(X_train.shape[0])
+
+        # Gradient descent (≈ 1 line of code)
+        parameters, grads, costs = self.optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost=True)
+        
+        # Retrieve parameters w and b from dictionary "parameters"
+        w = parameters["w"]
+        b = parameters["b"]
+        
+        # Predict test/train set examples (≈ 2 lines of code)
+        Y_prediction_test = self.predict(w, b, X_test)
+        Y_prediction_train = self.predict(w, b, X_train)
+
+        ### END CODE HERE ###
+
+        # Print train/test Errors
+        print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+        print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+        
+        d = {"costs": costs,
+             "Y_prediction_test": Y_prediction_test, 
+             "Y_prediction_train" : Y_prediction_train, 
+             "w" : w, 
+             "b" : b,
+             "learning_rate" : learning_rate,
+             "num_iterations": num_iterations}
+        
+        return d
+
     def Main(self):
         plt.switch_backend('Qt5Agg')
-        # 2 - Overview of the Problem set
+        '''
+        2 - Overview of the Problem set
+        '''
         train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = self.load_dataset()
         index = 16
 
@@ -118,8 +270,8 @@ class Coding1_1(CodingWorks):
         logging.info(msgString)
 
         # 显示训练集中的一张图片
-        plt.imshow(train_set_x_orig[index])
-        plt.show()
+        # plt.imshow(train_set_x_orig[index])
+        # plt.show()
 
         # 打印训练集、测试集的样本个数、像素数、通道数，以及标注数据的个数
         ### START CODE HERE ### (≈ 3 lines of code)
@@ -154,7 +306,14 @@ class Coding1_1(CodingWorks):
         train_set_x = train_set_x_flatten / 255.
         test_set_x = test_set_x_flatten / 255.
 
-        # 3 - General Architecture of the learning algorithm
+        d = self.model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+        index = 5
+        plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+        msgString = 'y = %s' % (str(test_set_y[0,index]))
+        msgString += ', you predicted that it is a %s picture.' % (classes["Y_prediction_test"][0,index].decode("utf-8"))
+        logging.info ( msgString)
+        plt.show()
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
