1e59a7709ec3c1c31adb9fe7eada391e66980b0c
palanceli <palanceli@163.com>
2018-03-15 21:36:01 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -366,7 +366,8 @@ class Coding1_2(CodingWorks):
         plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
 
 
-    def Main(self):
+    def LogisticRegression(self):
+        # 通过逻辑回归生成分类器，将红豆和绿豆分开
         # X是400个点，Y是400个颜色r/b值
         X, Y = self.load_planar_dataset()
         # 绘制这些点
@@ -395,6 +396,173 @@ class Coding1_2(CodingWorks):
 
 
         plt.show()
+
+    # 4.1 - Defining the neural network structure
+    def layer_sizes(self, X, Y):
+        """
+        Arguments:
+        X -- input dataset of shape (input size, number of examples)
+        Y -- labels of shape (output size, number of examples)
+        
+        Returns:
+        n_x -- the size of the input layer
+        n_h -- the size of the hidden layer
+        n_y -- the size of the output layer
+        """
+        ### START CODE HERE ### (≈ 3 lines of code)
+        n_x = X.shape[0] # size of input layer
+        n_h = 4
+        n_y = Y.shape[0] # size of output layer
+        ### END CODE HERE ###
+        return (n_x, n_h, n_y)
+
+    # 4.2 - Initialize the model's parameters
+    def initialize_parameters(self, n_x, n_h, n_y):
+        """
+        Argument:
+        n_x -- size of the input layer
+        n_h -- size of the hidden layer
+        n_y -- size of the output layer
+        
+        Returns:
+        params -- python dictionary containing your parameters:
+                        W1 -- weight matrix of shape (n_h, n_x)
+                        b1 -- bias vector of shape (n_h, 1)
+                        W2 -- weight matrix of shape (n_y, n_h)
+                        b2 -- bias vector of shape (n_y, 1)
+        """
+        
+        np.random.seed(2) # we set up a seed so that your output matches ours although the initialization is random.
+        
+        ### START CODE HERE ### (≈ 4 lines of code)
+        W1 = np.random.randn(n_h, n_x) * 0.01
+        b1 = np.zeros((n_h, 1))
+        W2 = np.random.randn(n_y, n_h) * 0.01
+        b2 = np.zeros((n_y, 1))
+        ### END CODE HERE ###
+        
+        assert (W1.shape == (n_h, n_x))
+        assert (b1.shape == (n_h, 1))
+        assert (W2.shape == (n_y, n_h))
+        assert (b2.shape == (n_y, 1))
+        
+        parameters = {"W1": W1,
+                      "b1": b1,
+                      "W2": W2,
+                      "b2": b2}
+        
+        return parameters
+
+    # 4.3 - The Loop
+    def forward_propagation(self, X, parameters):
+        """
+        Argument:
+        X -- input data of size (n_x, m)
+        parameters -- python dictionary containing your parameters (output of initialization function)
+        
+        Returns:
+        A2 -- The sigmoid output of the second activation
+        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+        """
+        # Retrieve each parameter from the dictionary "parameters"
+        ### START CODE HERE ### (≈ 4 lines of code)
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        ### END CODE HERE ###
+        
+        # Implement Forward Propagation to calculate A2 (probabilities)
+        ### START CODE HERE ### (≈ 4 lines of code)
+        Z1 = np.dot(W1,X) + b1
+        A1 = np.tanh(Z1)
+        Z2 = np.dot(W2,A1) + b2
+        A2 = sigmoid(Z2)
+        ### END CODE HERE ###
+        
+        assert(A2.shape == (1, X.shape[1]))
+        
+        cache = {"Z1": Z1,
+                 "A1": A1,
+                 "Z2": Z2,
+                 "A2": A2}
+        
+        return A2, cache
+
+    def compute_cost(self, A2, Y, parameters):
+        """
+        Computes the cross-entropy cost given in equation (13)
+        
+        Arguments:
+        A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+        Y -- "true" labels vector of shape (1, number of examples)
+        parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+        
+        Returns:
+        cost -- cross-entropy cost given equation (13)
+        """
+        
+        m = Y.shape[1] # number of example
+
+        # Compute the cross-entropy cost
+        ### START CODE HERE ### (≈ 2 lines of code)
+        logprobs = np.multiply(np.log(A2),Y)+np.multiply(1-Y,np.log(1-A2))
+        cost = - np.sum(logprobs) / m
+        ### END CODE HERE ###
+        
+        cost = np.squeeze(cost)     # makes sure cost is the dimension we expect. 
+                                    # E.g., turns [[17]] into 17 
+        assert(isinstance(cost, float))
+        
+        return cost
+
+    def backward_propagation(self, parameters, cache, X, Y):
+        """
+        Implement the backward propagation using the instructions above.
+        
+        Arguments:
+        parameters -- python dictionary containing our parameters 
+        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
+        X -- input data of shape (2, number of examples)
+        Y -- "true" labels vector of shape (1, number of examples)
+        
+        Returns:
+        grads -- python dictionary containing your gradients with respect to different parameters
+        """
+        m = X.shape[1]
+        
+        # First, retrieve W1 and W2 from the dictionary "parameters".
+        ### START CODE HERE ### (≈ 2 lines of code)
+        W1 = parameters["W1"]
+        W2 = parameters["W2"]
+        ### END CODE HERE ###
+            
+        # Retrieve also A1 and A2 from dictionary "cache".
+        ### START CODE HERE ### (≈ 2 lines of code)
+        A1 = cache["A1"]
+        A2 = cache["A2"]
+        ### END CODE HERE ###
+        
+        # Backward propagation: calculate dW1, db1, dW2, db2. 
+        ### START CODE HERE ### (≈ 6 lines of code, corresponding to 6 equations on slide above)
+        dZ2 = A2 - Y
+        dW2 = np.dot(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis = 1, keepdims = True) / m
+        dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1,2))
+        dW1 = np.dot(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis = 1, keepdims = True) / m
+        ### END CODE HERE ###
+        
+        grads = {"dW1": dW1,
+                 "db1": db1,
+                 "dW2": dW2,
+                 "db2": db2}
+        
+        return grads
+        
+    def Main(self):
+        # 使用双层神经网络生成分类器，将红豆和绿豆分开
+        pass
 
     def tc1(self):
         # 验证np.meshgrid的作用
