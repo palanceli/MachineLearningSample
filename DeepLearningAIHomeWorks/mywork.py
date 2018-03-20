fc4763408d94054d10d99c88b9426d3cab16aee1
palance <palanceli@163.com>
2018-03-20 22:04:11 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -20,7 +20,7 @@ from scipy import ndimage
 import sklearn
 import sklearn.datasets
 import sklearn.linear_model
-
+import scipy.io
 
 class CodingWorks(unittest.TestCase):
     def setUp(self):
@@ -1362,6 +1362,435 @@ class Coding1_3(CodingWorks):
         parameters = dnn_app_utils_v2.initialize_parameters_deep(layers_dims)
         arameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
         
+class Coding2_1_init(CodingWorks):
+    def load_dataset(self):
+        np.random.seed(1)
+        train_X, train_Y = sklearn.datasets.make_circles(n_samples=300, noise=.05)
+        np.random.seed(2)
+        test_X, test_Y = sklearn.datasets.make_circles(n_samples=100, noise=.05)
+        train_X = train_X.T
+        train_Y = train_Y.reshape((1, train_Y.shape[0]))
+        test_X = test_X.T
+        test_Y = test_Y.reshape((1, test_Y.shape[0]))
+        return train_X, train_Y, test_X, test_Y    
+        
+    def tc1(self):
+        '''  查看可视化的数据 '''
+        train_X, train_Y, test_X, test_Y  = self.load_dataset()
+        plt.scatter(train_X[0, :], train_X[1, :], c=train_Y.flatten(), s=40, cmap=plt.cm.Spectral);
+        plt.show()
+
+    def initialize_parameters_zeros(self, layers_dims):
+        """
+        Arguments:
+        layer_dims -- 各层节点个数.
+        
+        Returns:
+        parameters -- {W1: , b1: , ... , WL: , bL:}
+        """
+        
+        parameters = {}
+        L = len(layers_dims)            # number of layers in the network
+        
+        for l in range(1, L):
+            parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l-1]))
+            parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        return parameters
+
+    def tc2(self):
+        parameters = self.initialize_parameters_zeros([3,2,1])
+        print("W1 = " + str(parameters["W1"]))
+        print("b1 = " + str(parameters["b1"]))
+        print("W2 = " + str(parameters["W2"]))
+        print("b2 = " + str(parameters["b2"]))
+
+    def initialize_parameters_random(self, layers_dims):
+        """
+        Arguments:
+        layer_dims -- python array (list) containing the size of each layer.
+        
+        Returns:
+        parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                        W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                        b1 -- bias vector of shape (layers_dims[1], 1)
+                        ...
+                        WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                        bL -- bias vector of shape (layers_dims[L], 1)
+        """
+        
+        np.random.seed(3)               # This seed makes sure your "random" numbers will be the as ours
+        parameters = {}
+        L = len(layers_dims)            # integer representing the number of layers
+        
+        for l in range(1, L):
+            ### START CODE HERE ### (≈ 2 lines of code)
+            parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * 10;
+            parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+            ### END CODE HERE ###
+
+        return parameters
+
+    def tc3(self):
+        parameters = self.initialize_parameters_random([3, 2, 1])
+        print("W1 = " + str(parameters["W1"]))
+        print("b1 = " + str(parameters["b1"]))
+        print("W2 = " + str(parameters["W2"]))
+        print("b2 = " + str(parameters["b2"]))
+
+    def initialize_parameters_he(self, layers_dims):
+        """
+        Arguments:
+        layer_dims -- python array (list) containing the size of each layer.
+        
+        Returns:
+        parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                        W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                        b1 -- bias vector of shape (layers_dims[1], 1)
+                        ...
+                        WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                        bL -- bias vector of shape (layers_dims[L], 1)
+        """
+        
+        np.random.seed(3)
+        parameters = {}
+        L = len(layers_dims) - 1 # integer representing the number of layers
+         
+        for l in range(1, L + 1):
+            ### START CODE HERE ### (≈ 2 lines of code)
+            parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * np.sqrt(2./layers_dims[l-1])
+            parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+            ### END CODE HERE ###
+            
+        return parameters
+
+    def tc4(self):
+        parameters = self.initialize_parameters_he([2, 4, 1])
+        print("W1 = " + str(parameters["W1"]))
+        print("b1 = " + str(parameters["b1"]))
+        print("W2 = " + str(parameters["W2"]))
+        print("b2 = " + str(parameters["b2"]))
+
+    def sigmoid(self, x):
+        """
+        Compute the sigmoid of x
+
+        Arguments:
+        x -- A scalar or numpy array of any size.
+
+        Return:
+        s -- sigmoid(x)
+        """
+        s = 1/(1+np.exp(-x))
+        return s
+
+    def relu(self, x):
+        """
+        Compute the relu of x
+
+        Arguments:
+        x -- A scalar or numpy array of any size.
+
+        Return:
+        s -- relu(x)
+        """
+        s = np.maximum(0,x)
+        
+        return s
+
+    def forward_propagation(self, X, parameters):
+        """
+        Implements the forward propagation (and computes the loss) presented in Figure 2.
+        
+        Arguments:
+        X -- input dataset, of shape (input size, number of examples)
+        Y -- true "label" vector (containing 0 if cat, 1 if non-cat)
+        parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                        W1 -- weight matrix of shape ()
+                        b1 -- bias vector of shape ()
+                        W2 -- weight matrix of shape ()
+                        b2 -- bias vector of shape ()
+                        W3 -- weight matrix of shape ()
+                        b3 -- bias vector of shape ()
+        
+        Returns:
+        loss -- the loss function (vanilla logistic loss)
+        """
+            
+        # retrieve parameters
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        W3 = parameters["W3"]
+        b3 = parameters["b3"]
+        
+        # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+        z1 = np.dot(W1, X) + b1
+        a1 = self.relu(z1)
+        z2 = np.dot(W2, a1) + b2
+        a2 = self.relu(z2)
+        z3 = np.dot(W3, a2) + b3
+        a3 = self.sigmoid(z3)
+        
+        cache = (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3)
+        
+        return a3, cache
+
+    def backward_propagation(self, X, Y, cache):
+        """
+        Implement the backward propagation presented in figure 2.
+        
+        Arguments:
+        X -- input dataset, of shape (input size, number of examples)
+        Y -- true "label" vector (containing 0 if cat, 1 if non-cat)
+        cache -- cache output from forward_propagation()
+        
+        Returns:
+        gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+        """
+        m = X.shape[1]
+        (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3) = cache
+        
+        dz3 = 1./m * (a3 - Y)
+        dW3 = np.dot(dz3, a2.T)
+        db3 = np.sum(dz3, axis=1, keepdims = True)
+        
+        da2 = np.dot(W3.T, dz3)
+        dz2 = np.multiply(da2, np.int64(a2 > 0))
+        dW2 = np.dot(dz2, a1.T)
+        db2 = np.sum(dz2, axis=1, keepdims = True)
+        
+        da1 = np.dot(W2.T, dz2)
+        dz1 = np.multiply(da1, np.int64(a1 > 0))
+        dW1 = np.dot(dz1, X.T)
+        db1 = np.sum(dz1, axis=1, keepdims = True)
+        
+        gradients = {"dz3": dz3, "dW3": dW3, "db3": db3,
+                     "da2": da2, "dz2": dz2, "dW2": dW2, "db2": db2,
+                     "da1": da1, "dz1": dz1, "dW1": dW1, "db1": db1}
+        
+        return gradients
+
+    def update_parameters(self, parameters, grads, learning_rate):
+        """
+        Update parameters using gradient descent
+        
+        Arguments:
+        parameters -- python dictionary containing your parameters 
+        grads -- python dictionary containing your gradients, output of n_model_backward
+        
+        Returns:
+        parameters -- python dictionary containing your updated parameters 
+                      parameters['W' + str(i)] = ... 
+                      parameters['b' + str(i)] = ...
+        """
+        
+        L = len(parameters) // 2 # number of layers in the neural networks
+
+        # Update rule for each parameter
+        for k in range(L):
+            parameters["W" + str(k+1)] = parameters["W" + str(k+1)] - learning_rate * grads["dW" + str(k+1)]
+            parameters["b" + str(k+1)] = parameters["b" + str(k+1)] - learning_rate * grads["db" + str(k+1)]
+            
+        return parameters
+
+    def compute_loss(self, a3, Y):
+        
+        """
+        Implement the loss function
+        
+        Arguments:
+        a3 -- post-activation, output of forward propagation
+        Y -- "true" labels vector, same shape as a3
+        
+        Returns:
+        loss - value of the loss function
+        """
+        
+        m = Y.shape[1]
+        logprobs = np.multiply(-np.log(a3),Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+        loss = 1./m * np.nansum(logprobs)
+        
+        return loss
+
+    def model(self, X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+        """
+        实现三层神经网络: LINEAR->RELU->LINEAR->RELU->LINEAR->SIGMOID.
+        
+        Arguments: 略
+        
+        Returns:
+        parameters -- parameters learnt by the model
+        """
+            
+        grads = {}
+        costs = [] # to keep track of the loss
+        m = X.shape[1] # number of examples
+        layers_dims = [X.shape[0], 10, 5, 1]
+        
+        # Initialize parameters dictionary.
+        if initialization == "zeros":
+            parameters = self.initialize_parameters_zeros(layers_dims)
+        elif initialization == "random":
+            parameters = self.initialize_parameters_random(layers_dims)
+        elif initialization == "he":
+            parameters = self.initialize_parameters_he(layers_dims)
+
+        # Loop (gradient descent)
+
+        for i in range(0, num_iterations):
+
+            # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+            a3, cache = self.forward_propagation(X, parameters)
+            
+            # Loss
+            cost = self.compute_loss(a3, Y)
+
+            # Backward propagation.
+            grads = self.backward_propagation(X, Y, cache)
+            
+            # Update parameters.
+            parameters = self.update_parameters(parameters, grads, learning_rate)
+            
+            # Print the loss every 1000 iterations
+            if print_cost and i % 1000 == 0:
+                print("Cost after iteration {}: {}".format(i, cost))
+                costs.append(cost)
+                
+        # plot the loss
+        plt.plot(costs)
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per hundreds)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+        
+        return parameters
+
+    def predict(self, X, y, parameters):
+        """
+        This function is used to predict the results of a  n-layer neural network.
+        
+        Arguments:
+        X -- data set of examples you would like to label
+        parameters -- parameters of the trained model
+        
+        Returns:
+        p -- predictions for the given dataset X
+        """
+        
+        m = X.shape[1]
+        p = np.zeros((1,m), dtype = np.int)
+        
+        # Forward propagation
+        a3, caches = self.forward_propagation(X, parameters)
+        
+        # convert probas to 0/1 predictions
+        for i in range(0, a3.shape[1]):
+            if a3[0,i] > 0.5:
+                p[0,i] = 1
+            else:
+                p[0,i] = 0
+
+        # print results
+        print("Accuracy: "  + str(np.mean((p[0,:] == y[0,:]))))
+        
+        return p
+
+    def tc5(self):
+        train_X, train_Y, test_X, test_Y = self.load_dataset()
+        parameters = self.model(train_X, train_Y, initialization = "zeros")
+        print ("On the train set:")
+        predictions_train = self.predict(train_X, train_Y, parameters)
+        print ("On the test set:")
+        predictions_test = self.predict(test_X, test_Y, parameters)
+
+        plt.title("Model with large random initialization")
+        axes = plt.gca()
+        axes.set_xlim([-1.5,1.5])
+        axes.set_ylim([-1.5,1.5])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+    def plot_decision_boundary(self, model, X, y):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+        y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+        h = 0.01
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        # Predict the function value for the whole grid
+        Z = model(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+        plt.ylabel('x2')
+        plt.xlabel('x1')
+        plt.scatter(X[0, :], X[1, :], c=y.flatten(), cmap=plt.cm.Spectral)
+        plt.show()
+
+    def predict_dec(self, parameters, X):
+        """
+        Used for plotting decision boundary.
+        
+        Arguments:
+        parameters -- python dictionary containing your parameters 
+        X -- input data of size (m, K)
+        
+        Returns
+        predictions -- vector of predictions of our model (red: 0 / blue: 1)
+        """
+        
+        # Predict using forward propagation and a classification threshold of 0.5
+        a3, cache = self.forward_propagation(X, parameters)
+        predictions = (a3>0.5)
+        return predictions
+
+    def tc6(self):
+        train_X, train_Y, test_X, test_Y = self.load_dataset()
+        parameters = self.model(train_X, train_Y, initialization = "random")
+        print ("On the train set:")
+        predictions_train = self.predict(train_X, train_Y, parameters)
+        print ("On the test set:")
+        predictions_test = self.predict(test_X, test_Y, parameters)
+
+        plt.title("Model with large random initialization")
+        axes = plt.gca()
+        axes.set_xlim([-1.5,1.5])
+        axes.set_ylim([-1.5,1.5])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+    def tc7(self):
+        train_X, train_Y, test_X, test_Y = self.load_dataset()
+        parameters = self.model(train_X, train_Y, initialization = "he")
+        print ("On the train set:")
+        predictions_train = self.predict(train_X, train_Y, parameters)
+        print ("On the test set:")
+        predictions_test = self.predict(test_X, test_Y, parameters)
+
+        plt.title("Model with He initialization")
+        axes = plt.gca()
+        axes.set_xlim([-1.5,1.5])
+        axes.set_ylim([-1.5,1.5])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+class Coding2_1_reg(CodingWorks):
+    def setUp(self):
+        super().setUp()
+        self.dataPath = os.path.join(self.rootDir, 'coding2_1', 'datasets/data.mat')
+        
+    def load_2D_dataset(self):
+        data = scipy.io.loadmat(self.dataPath)
+        train_X = data['X'].T
+        train_Y = data['y'].T
+        test_X = data['Xval'].T
+        test_Y = data['yval'].T
+
+        return train_X, train_Y, test_X, test_Y
+
+    def tc1(self):
+        ''' 数据可视化 '''
+        train_X, train_Y, test_X, test_Y = self.load_2D_dataset()
+        plt.scatter(train_X[0, :], train_X[1, :], c=train_Y.flatten(), s=40, cmap=plt.cm.Spectral);
+        plt.show()
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
