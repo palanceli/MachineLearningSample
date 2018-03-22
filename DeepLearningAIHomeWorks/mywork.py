6a7aad52ceb096ccd31c26cbd3ebb42cd93da79e
palance <palanceli@163.com>
2018-03-22 10:34:56 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -1968,6 +1968,175 @@ class Coding2_1_reg(CodingWorks):
             
         return parameters
 
+    def compute_cost_with_regularization(self, A3, Y, parameters, lambd):
+        """
+        Implement the cost function with L2 regularization. See formula (2) above.
+        
+        Arguments:
+        A3 -- post-activation, output of forward propagation, of shape (output size, number of examples)
+        Y -- "true" labels vector, of shape (output size, number of examples)
+        parameters -- python dictionary containing parameters of the model
+        
+        Returns:
+        cost - value of the regularized loss function (formula (2))
+        """
+        m = Y.shape[1]
+        W1 = parameters["W1"]
+        W2 = parameters["W2"]
+        W3 = parameters["W3"]
+        
+        cross_entropy_cost = self.compute_cost(A3, Y) # This gives you the cross-entropy part of the cost
+        
+        ### START CODE HERE ### (approx. 1 line)
+        L2_regularization_cost = (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3))) * lambd / (2 * m)
+        ### END CODER HERE ###
+        
+        cost = cross_entropy_cost + L2_regularization_cost
+        
+        return cost
+
+    def backward_propagation_with_regularization(self, X, Y, cache, lambd):
+        """
+        Implements the backward propagation of our baseline model to which we added an L2 regularization.
+        
+        Arguments:
+        X -- input dataset, of shape (input size, number of examples)
+        Y -- "true" labels vector, of shape (output size, number of examples)
+        cache -- cache output from forward_propagation()
+        lambd -- regularization hyperparameter, scalar
+        
+        Returns:
+        gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+        """
+        
+        m = X.shape[1]
+        (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+        
+        dZ3 = A3 - Y
+        
+        ### START CODE HERE ### (approx. 1 line)
+        dW3 = 1./m * np.dot(dZ3, A2.T) + lambd * W3 / m
+        ### END CODE HERE ###
+        db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+        
+        dA2 = np.dot(W3.T, dZ3)
+        dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+        ### START CODE HERE ### (approx. 1 line)
+        dW2 = 1./m * np.dot(dZ2, A1.T) + lambd * W2 / m
+        ### END CODE HERE ###
+        db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+        
+        dA1 = np.dot(W2.T, dZ2)
+        dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+        ### START CODE HERE ### (approx. 1 line)
+        dW1 = 1./m * np.dot(dZ1, X.T) + lambd * W1 / m
+        ### END CODE HERE ###
+        db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+        
+        gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                     "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                     "dZ1": dZ1, "dW1": dW1, "db1": db1}
+        
+        return gradients
+
+    def forward_propagation_with_dropout(self, X, parameters, keep_prob = 0.5):
+        """
+        Implements the forward propagation: LINEAR -> RELU + DROPOUT -> LINEAR -> RELU + DROPOUT -> LINEAR -> SIGMOID.
+        
+        Arguments:
+        X -- input dataset, of shape (2, number of examples)
+        parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                        W1 -- weight matrix of shape (20, 2)
+                        b1 -- bias vector of shape (20, 1)
+                        W2 -- weight matrix of shape (3, 20)
+                        b2 -- bias vector of shape (3, 1)
+                        W3 -- weight matrix of shape (1, 3)
+                        b3 -- bias vector of shape (1, 1)
+        keep_prob - probability of keeping a neuron active during drop-out, scalar
+        
+        Returns:
+        A3 -- last activation value, output of the forward propagation, of shape (1,1)
+        cache -- tuple, information stored for computing the backward propagation
+        """
+        
+        np.random.seed(1)
+        
+        # retrieve parameters
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        W3 = parameters["W3"]
+        b3 = parameters["b3"]
+        
+        # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+        Z1 = np.dot(W1, X) + b1
+        A1 = self.relu(Z1)
+        ### START CODE HERE ### (approx. 4 lines)         # Steps 1-4 below correspond to the Steps 1-4 described above. 
+        D1 = np.random.rand(A1.shape[0], A1.shape[1])     # Step 1: initialize matrix D1 = np.random.rand(..., ...)
+        D1 = (D1 < keep_prob)                             # Step 2: convert entries of D1 to 0 or 1 (using keep_prob as the threshold)
+        A1 = np.multiply(A1, D1)                          # Step 3: shut down some neurons of A1
+        A1 = A1 / keep_prob                               # Step 4: scale the value of neurons that haven't been shut down
+        ### END CODE HERE ###
+        Z2 = np.dot(W2, A1) + b2
+        A2 = self.relu(Z2)
+        ### START CODE HERE ### (approx. 4 lines)
+        D2 = np.random.rand(A2.shape[0], A2.shape[1])     # Step 1: initialize matrix D2 = np.random.rand(..., ...)
+        D2 = (D2 < keep_prob)                             # Step 2: convert entries of D2 to 0 or 1 (using keep_prob as the threshold)
+        A2 = np.multiply(A2, D2)                          # Step 3: shut down some neurons of A2
+        A2 = A2 / keep_prob                               # Step 4: scale the value of neurons that haven't been shut down
+        ### END CODE HERE ###
+        Z3 = np.dot(W3, A2) + b3
+        A3 = self.sigmoid(Z3)
+        
+        cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+        
+        return A3, cache
+
+    def backward_propagation_with_dropout(self, X, Y, cache, keep_prob):
+        """
+        Implements the backward propagation of our baseline model to which we added dropout.
+        
+        Arguments:
+        X -- input dataset, of shape (2, number of examples)
+        Y -- "true" labels vector, of shape (output size, number of examples)
+        cache -- cache output from forward_propagation_with_dropout()
+        keep_prob - probability of keeping a neuron active during drop-out, scalar
+        
+        Returns:
+        gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+        """
+        
+        m = X.shape[1]
+        (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+        
+        dZ3 = A3 - Y
+        dW3 = 1./m * np.dot(dZ3, A2.T)
+        db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+        dA2 = np.dot(W3.T, dZ3)
+        ### START CODE HERE ### (≈ 2 lines of code)
+        dA2 = np.multiply(dA2, D2)              # Step 1: Apply mask D2 to shut down the same neurons as during the forward propagation
+        dA2 = dA2 / keep_prob              # Step 2: Scale the value of neurons that haven't been shut down
+        ### END CODE HERE ###
+        dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+        dW2 = 1./m * np.dot(dZ2, A1.T)
+        db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+        
+        dA1 = np.dot(W2.T, dZ2)
+        ### START CODE HERE ### (≈ 2 lines of code)
+        dA1 = np.multiply(dA1, D1)              # Step 1: Apply mask D1 to shut down the same neurons as during the forward propagation
+        dA1 = dA1 / keep_prob              # Step 2: Scale the value of neurons that haven't been shut down
+        ### END CODE HERE ###
+        dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+        dW1 = 1./m * np.dot(dZ1, X.T)
+        db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+        
+        gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                     "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                     "dZ1": dZ1, "dW1": dW1, "db1": db1}
+        
+        return gradients
+
     def model(self, X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, lambd = 0, keep_prob = 1):
         """
         Implements a three-layer neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SIGMOID.
@@ -2001,13 +2170,13 @@ class Coding2_1_reg(CodingWorks):
             if keep_prob == 1:
                 a3, cache = self.forward_propagation(X, parameters)
             elif keep_prob < 1:
-                a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+                a3, cache = self.forward_propagation_with_dropout(X, parameters, keep_prob)
             
             # Cost function
             if lambd == 0:
                 cost = self.compute_cost(a3, Y)
             else:
-                cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+                cost = self.compute_cost_with_regularization(a3, Y, parameters, lambd)
                 
             # Backward propagation.
             assert(lambd==0 or keep_prob==1)    # it is possible to use both L2 regularization and dropout, 
@@ -2015,9 +2184,9 @@ class Coding2_1_reg(CodingWorks):
             if lambd == 0 and keep_prob == 1:
                 grads = self.backward_propagation(X, Y, cache)
             elif lambd != 0:
-                grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+                grads = self.backward_propagation_with_regularization(X, Y, cache, lambd)
             elif keep_prob < 1:
-                grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+                grads = self.backward_propagation_with_dropout(X, Y, cache, keep_prob)
             
             # Update parameters.
             parameters = self.update_parameters(parameters, grads, learning_rate)
@@ -2070,6 +2239,40 @@ class Coding2_1_reg(CodingWorks):
         
         return p
 
+    def plot_decision_boundary(self, model, X, y):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+        y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+        h = 0.01
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        # Predict the function value for the whole grid
+        Z = model(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+        plt.ylabel('x2')
+        plt.xlabel('x1')
+        plt.scatter(X[0, :], X[1, :], c=y.flatten(), cmap=plt.cm.Spectral)
+        plt.show()
+            
+    def predict_dec(self, parameters, X):
+        """
+        Used for plotting decision boundary.
+        
+        Arguments:
+        parameters -- python dictionary containing your parameters 
+        X -- input data of size (m, K)
+        
+        Returns
+        predictions -- vector of predictions of our model (red: 0 / blue: 1)
+        """
+        
+        # Predict using forward propagation and a classification threshold of 0.5
+        a3, cache = self.forward_propagation(X, parameters)
+        predictions = (a3>0.5)
+        return predictions
+
     def tc2(self):
         train_X, train_Y, test_X, test_Y = self.load_2D_dataset()
         parameters = self.model(train_X, train_Y)
@@ -2077,6 +2280,60 @@ class Coding2_1_reg(CodingWorks):
         predictions_train = self.predict(train_X, train_Y, parameters)
         logging.info ("On the test set:")
         predictions_test = self.predict(test_X, test_Y, parameters)
+
+        plt.title("Model without regularization")
+        axes = plt.gca()
+        axes.set_xlim([-0.75,0.40])
+        axes.set_ylim([-0.75,0.65])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+    def tc3(self):
+        train_X, train_Y, test_X, test_Y = self.load_2D_dataset()
+        parameters = self.model(train_X, train_Y, lambd = 0.7)
+        print ("On the train set:")
+        predictions_train = self.predict(train_X, train_Y, parameters)
+        print ("On the test set:")
+        predictions_test = self.predict(test_X, test_Y, parameters)
+
+        plt.title("Model without regularization")
+        axes = plt.gca()
+        axes.set_xlim([-0.75,0.40])
+        axes.set_ylim([-0.75,0.65])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+    def tc4(self):
+        train_X, train_Y, test_X, test_Y = self.load_2D_dataset()
+        parameters = self.model(train_X, train_Y, keep_prob = 0.86, learning_rate = 0.3)
+
+        print ("On the train set:")
+        predictions_train = self.predict(train_X, train_Y, parameters)
+        print ("On the test set:")
+        predictions_test = self.predict(test_X, test_Y, parameters)
+
+        plt.title("Model without regularization")
+        axes = plt.gca()
+        axes.set_xlim([-0.75,0.40])
+        axes.set_ylim([-0.75,0.65])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+class Coding2_1_gc(CodingWorks):
+    def forward_propagation(self, x, theta):
+        """
+        Implement the linear forward propagation (compute J) presented in Figure 1 (J(theta) = theta * x)
+        
+        Arguments:
+        x -- a real-valued input
+        theta -- our parameter, a real number as well
+        
+        Returns:
+        J -- the value of function J, computed using the formula J(theta) = theta * x
+        """
+        
+        ### START CODE HERE ### (approx. 1 line)
+        J = theta * x
+        ### END CODE HERE ###
+        
+        return J
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
