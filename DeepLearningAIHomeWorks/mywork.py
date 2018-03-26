720954c8c19213d74e7d119d63acda369f86d1be
palanceli <palanceli@163.com>
2018-03-26 22:19:05 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -2373,23 +2373,283 @@ class Coding2_1_reg(CodingWorks):
         self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
 
 class Coding2_1_gc(CodingWorks):
-    def forward_propagation(self, x, theta):
+
+    def sigmoid(self, x):
         """
-        Implement the linear forward propagation (compute J) presented in Figure 1 (J(theta) = theta * x)
+        Compute the sigmoid of x
+
+        Arguments:
+        x -- A scalar or numpy array of any size.
+
+        Return:
+        s -- sigmoid(x)
+        """
+        s = 1/(1+np.exp(-x))
+        return s
+
+    def relu(self, x):
+        """
+        Compute the relu of x
+
+        Arguments:
+        x -- A scalar or numpy array of any size.
+
+        Return:
+        s -- relu(x)
+        """
+        s = np.maximum(0,x)
+        
+        return s
+
+    def forward_propagation_n(self, X, Y, parameters):
+        """
+        Implements the forward propagation (and computes the cost) presented in Figure 3.
         
         Arguments:
-        x -- a real-valued input
-        theta -- our parameter, a real number as well
+        X -- training set for m examples
+        Y -- labels for m examples 
+        parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                        W1 -- weight matrix of shape (5, 4)
+                        b1 -- bias vector of shape (5, 1)
+                        W2 -- weight matrix of shape (3, 5)
+                        b2 -- bias vector of shape (3, 1)
+                        W3 -- weight matrix of shape (1, 3)
+                        b3 -- bias vector of shape (1, 1)
         
         Returns:
-        J -- the value of function J, computed using the formula J(theta) = theta * x
+        cost -- the cost function (logistic cost for one example)
         """
         
-        ### START CODE HERE ### (approx. 1 line)
-        J = theta * x
-        ### END CODE HERE ###
+        # retrieve parameters
+        m = X.shape[1]
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        W3 = parameters["W3"]
+        b3 = parameters["b3"]
+
+        # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+        Z1 = np.dot(W1, X) + b1
+        A1 = self.relu(Z1)
+        Z2 = np.dot(W2, A1) + b2
+        A2 = self.relu(Z2)
+        Z3 = np.dot(W3, A2) + b3
+        A3 = self.sigmoid(Z3)
+
+        # Cost
+        logprobs = np.multiply(-np.log(A3),Y) + np.multiply(-np.log(1 - A3), 1 - Y)
+        cost = 1./m * np.sum(logprobs)
         
-        return J
+        cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+        
+        return cost, cache
+    
+    def backward_propagation_n(self, X, Y, cache):
+        """
+        Implement the backward propagation presented in figure 2.
+        
+        Arguments:
+        X -- input datapoint, of shape (input size, 1)
+        Y -- true "label"
+        cache -- cache output from forward_propagation_n()
+        
+        Returns:
+        gradients -- A dictionary with the gradients of the cost with respect to each parameter, activation and pre-activation variables.
+        """
+        
+        m = X.shape[1]
+        (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+        
+        dZ3 = A3 - Y
+        dW3 = 1./m * np.dot(dZ3, A2.T)
+        db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+        
+        dA2 = np.dot(W3.T, dZ3)
+        dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+        dW2 = 1./m * np.dot(dZ2, A1.T)
+        db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+        
+        dA1 = np.dot(W2.T, dZ2)
+        dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+        dW1 = 1./m * np.dot(dZ1, X.T)
+        db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+        
+        gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
+                    "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
+                    "dA1": dA1, "dZ1": dZ1, "dW1": dW1, "db1": db1}
+        
+        return gradients
+
+    def gradient_check_n_test_case(self): 
+        np.random.seed(1)
+        x = np.random.randn(4,3)
+        y = np.array([1, 1, 0])
+        W1 = np.random.randn(5,4) 
+        b1 = np.random.randn(5,1) 
+        W2 = np.random.randn(3,5) 
+        b2 = np.random.randn(3,1) 
+        W3 = np.random.randn(1,3) 
+        b3 = np.random.randn(1,1) 
+        parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2, "W3": W3, "b3": b3}
+
+        return x, y, parameters
+        
+    def vector_to_dictionary(self, theta):
+        """
+        Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
+        """
+        parameters = {}
+        parameters["W1"] = theta[:20].reshape((5,4))
+        parameters["b1"] = theta[20:25].reshape((5,1))
+        parameters["W2"] = theta[25:40].reshape((3,5))
+        parameters["b2"] = theta[40:43].reshape((3,1))
+        parameters["W3"] = theta[43:46].reshape((1,3))
+        parameters["b3"] = theta[46:47].reshape((1,1))
+
+        return parameters
+
+    def gradients_to_vector(self, gradients):
+        """
+        Roll all our gradients dictionary into a single vector satisfying our specific required shape.
+        """
+        
+        count = 0
+        for key in ["dW1", "db1", "dW2", "db2", "dW3", "db3"]:
+            # flatten parameter
+            new_vector = np.reshape(gradients[key], (-1,1))
+            
+            if count == 0:
+                theta = new_vector
+            else:
+                theta = np.concatenate((theta, new_vector), axis=0)
+            count = count + 1
+
+        return theta
+
+    def dictionary_to_vector(self, parameters):
+        """
+        Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+        """
+        keys = []
+        count = 0
+        for key in ["W1", "b1", "W2", "b2", "W3", "b3"]:
+            
+            # flatten parameter
+            new_vector = np.reshape(parameters[key], (-1,1))
+            keys = keys + [key]*new_vector.shape[0]
+            
+            if count == 0:
+                theta = new_vector
+            else:
+                theta = np.concatenate((theta, new_vector), axis=0)
+            count = count + 1
+
+        return theta, keys
+
+    def gradient_check_n(self, parameters, gradients, X, Y, epsilon = 1e-7):
+        """
+        Checks if backward_propagation_n computes correctly the gradient of the cost output by forward_propagation_n
+        
+        Arguments:
+        parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+        grad -- output of backward_propagation_n, contains gradients of the cost with respect to the parameters. 
+        x -- input datapoint, of shape (input size, 1)
+        y -- true "label"
+        epsilon -- tiny shift to the input to compute approximated gradient with formula(1)
+        
+        Returns:
+        difference -- difference (2) between the approximated gradient and the backward propagation gradient
+        """
+        
+        # Set-up variables
+        # 将W1 b1 W2 b2 W3 b3扁平化为47×1
+        parameters_values, _ = self.dictionary_to_vector(parameters)
+        # logging.info(parameters_values)
+        # logging.info(parameters_values.shape)
+
+        # 将dW1 db1 dW2 db2 dW3 db3扁平化为47×1
+        grad = self.gradients_to_vector(gradients)
+        # logging.info(grad)
+        # logging.info(grad.shape)
+
+        num_parameters = parameters_values.shape[0]
+        J_plus = np.zeros((num_parameters, 1))
+        J_minus = np.zeros((num_parameters, 1))
+        gradapprox = np.zeros((num_parameters, 1))
+        
+        # Compute gradapprox
+        for i in range(num_parameters):
+            
+            # Compute J_plus[i]. Inputs: "parameters_values, epsilon". Output = "J_plus[i]".
+            # "_" is used because the function you have to outputs two parameters but we only care about the first one
+            ### START CODE HERE ### (approx. 3 lines)
+            thetaplus = np.copy(parameters_values)                            # Step 1
+            thetaplus[i][0] = thetaplus[i][0] + epsilon                       # Step 2
+            J_plus[i], _ = self.forward_propagation_n(X, Y, self.vector_to_dictionary(thetaplus))                                   # Step 3
+            ### END CODE HERE ###
+            
+            # Compute J_minus[i]. Inputs: "parameters_values, epsilon". Output = "J_minus[i]".
+            ### START CODE HERE ### (approx. 3 lines)
+            thetaminus = np.copy(parameters_values)                           # Step 1
+            thetaminus[i][0] = thetaminus[i][0] - epsilon                     # Step 2        
+            J_minus[i], _ = self.forward_propagation_n(X, Y, self.vector_to_dictionary(thetaminus))                                  # Step 3
+            ### END CODE HERE ###
+            
+            # Compute gradapprox[i]
+            ### START CODE HERE ### (approx. 1 line)
+            gradapprox[i] = (J_plus[i] - J_minus[i]) / (2 * epsilon)
+            ### END CODE HERE ###
+        
+        # Compare gradapprox to backward propagation gradients by computing difference.
+        ### START CODE HERE ### (approx. 1 line)
+        numerator = np.linalg.norm(grad - gradapprox)                         # Step 1'
+        denominator = np.linalg.norm(grad) + np.linalg.norm(gradapprox)       # Step 2'
+        difference = numerator / denominator                                  # Step 3'
+        ### END CODE HERE ###
+
+        if difference > 1e-7:
+            print ("\033[93m" + "There is a mistake in the backward propagation! difference = " + str(difference) + "\033[0m")
+        else:
+            print ("\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
+        
+        return difference
+
+    def tc1(self):
+        # 随机生成X(4×3)、Y(1×3)、W1(5×4)、b1(5×1)、W2(3×5)、b2(3×1)、W3(1×3)、b3(1×1)
+        X, Y, parameters = self.gradient_check_n_test_case()
+        logging.info('X:%s' % X)
+        logging.info('Y:%s' % Y)
+
+        # 完成正向传播算法，返回cost, (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+        cost, cache = self.forward_propagation_n(X, Y, parameters)
+        # logging.info('Z1:%s' % cache[0])
+        # logging.info('A1:%s' % cache[1])
+        # logging.info('W1:%s' % cache[2])
+        # logging.info('b1:%s' % cache[3])
+        # logging.info('Z2:%s' % cache[4])
+        # logging.info('A2:%s' % cache[5])
+        # logging.info('W2:%s' % cache[6])
+        # logging.info('b2:%s' % cache[7])
+        # logging.info('Z3:%s' % cache[8])
+        # logging.info('A3:%s' % cache[9])
+        # logging.info('W3:%s' % cache[10])
+        # logging.info('b3:%s' % cache[11])
+        # 完成反向传播算法，返回{dZ3, dW3, db3, dA2, dZ2, dW2, db2, dA1, dZ1, dW1, db1}
+        gradients = self.backward_propagation_n(X, Y, cache)
+        # logging.info('dZ3:%s' % gradients['dZ3'])
+        # logging.info('dW3:%s' % gradients['dW3'])
+        # logging.info('db3:%s' % gradients['db3'])
+        # logging.info('dA2:%s' % gradients['dA2'])
+        # logging.info('dZ2:%s' % gradients['dZ2'])
+        # logging.info('dW2:%s' % gradients['dW2'])
+        # logging.info('db2:%s' % gradients['db2'])
+        # logging.info('dA1:%s' % gradients['dA1'])
+        # logging.info('dZ1:%s' % gradients['dZ1'])
+        # logging.info('dW1:%s' % gradients['dW1'])
+        # logging.info('db1:%s' % gradients['db1'])
+        difference = self.gradient_check_n(parameters, gradients, X, Y)
+
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
