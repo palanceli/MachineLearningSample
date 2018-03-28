d4bb98b059fc3eca40eec2dfaeb0ae3bb57f7753
palanceli <palanceli@163.com>
2018-03-28 21:57:23 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -3014,7 +3014,203 @@ class Coding2_2(CodingWorks):
         plt.scatter(train_X[0, :], train_X[1, :], c=train_Y.flatten(), s=20, cmap=plt.cm.Spectral);
         plt.show()
 
-    def model(X, Y, layers_dims, optimizer, learning_rate = 0.0007, mini_batch_size = 64, beta = 0.9,
+    def initialize_parameters(self, layer_dims):
+        """
+        Arguments:
+        layer_dims -- python array (list) containing the dimensions of each layer in our network
+        
+        Returns:
+        parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                        W1 -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                        b1 -- bias vector of shape (layer_dims[l], 1)
+                        Wl -- weight matrix of shape (layer_dims[l-1], layer_dims[l])
+                        bl -- bias vector of shape (1, layer_dims[l])
+                        
+        Tips:
+        - For example: the layer_dims for the "Planar Data classification model" would have been [2,2,1]. 
+        This means W1's shape was (2,2), b1 was (1,2), W2 was (2,1) and b2 was (1,1). Now you have to generalize it!
+        - In the for loop, use parameters['W' + str(l)] to access Wl, where l is the iterative integer.
+        """
+        
+        np.random.seed(3)
+        parameters = {}
+        L = len(layer_dims) # number of layers in the network
+
+        for l in range(1, L):
+            parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])*  np.sqrt(2 / layer_dims[l-1])
+            parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+            
+            assert(parameters['W' + str(l)].shape == layer_dims[l], layer_dims[l-1])
+            assert(parameters['W' + str(l)].shape == layer_dims[l], 1)
+            
+        return parameters
+
+    def forward_propagation(self, X, parameters):
+        """
+        Implements the forward propagation (and computes the loss) presented in Figure 2.
+        
+        Arguments:
+        X -- input dataset, of shape (input size, number of examples)
+        parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                        W1 -- weight matrix of shape ()
+                        b1 -- bias vector of shape ()
+                        W2 -- weight matrix of shape ()
+                        b2 -- bias vector of shape ()
+                        W3 -- weight matrix of shape ()
+                        b3 -- bias vector of shape ()
+        
+        Returns:
+        loss -- the loss function (vanilla logistic loss)
+        """
+        
+        # retrieve parameters
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+        W3 = parameters["W3"]
+        b3 = parameters["b3"]
+        
+        # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+        z1 = np.dot(W1, X) + b1
+        a1 = self.relu(z1)
+        z2 = np.dot(W2, a1) + b2
+        a2 = self.relu(z2)
+        z3 = np.dot(W3, a2) + b3
+        a3 = self.sigmoid(z3)
+        
+        cache = (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3)
+        
+        return a3, cache
+
+    def backward_propagation(self, X, Y, cache):
+        """
+        Implement the backward propagation presented in figure 2.
+        
+        Arguments:
+        X -- input dataset, of shape (input size, number of examples)
+        Y -- true "label" vector (containing 0 if cat, 1 if non-cat)
+        cache -- cache output from forward_propagation()
+        
+        Returns:
+        gradients -- A dictionary with the gradients with respect to each parameter, activation and pre-activation variables
+        """
+        m = X.shape[1]
+        (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3) = cache
+        
+        dz3 = 1./m * (a3 - Y)
+        dW3 = np.dot(dz3, a2.T)
+        db3 = np.sum(dz3, axis=1, keepdims = True)
+        
+        da2 = np.dot(W3.T, dz3)
+        dz2 = np.multiply(da2, np.int64(a2 > 0))
+        dW2 = np.dot(dz2, a1.T)
+        db2 = np.sum(dz2, axis=1, keepdims = True)
+        
+        da1 = np.dot(W2.T, dz2)
+        dz1 = np.multiply(da1, np.int64(a1 > 0))
+        dW1 = np.dot(dz1, X.T)
+        db1 = np.sum(dz1, axis=1, keepdims = True)
+        
+        gradients = {"dz3": dz3, "dW3": dW3, "db3": db3,
+                    "da2": da2, "dz2": dz2, "dW2": dW2, "db2": db2,
+                    "da1": da1, "dz1": dz1, "dW1": dW1, "db1": db1}
+        
+        return gradients
+
+    def sigmoid(self, x):
+        """
+        Compute the sigmoid of x
+
+        Arguments:
+        x -- A scalar or numpy array of any size.
+
+        Return:
+        s -- sigmoid(x)
+        """
+        s = 1/(1+np.exp(-x))
+        return s
+
+    def relu(self, x):
+        """
+        Compute the relu of x
+
+        Arguments:
+        x -- A scalar or numpy array of any size.
+
+        Return:
+        s -- relu(x)
+        """
+        s = np.maximum(0,x)
+        
+        return s
+
+    def compute_cost(self, a3, Y):
+        
+        """
+        Implement the cost function
+        
+        Arguments:
+        a3 -- post-activation, output of forward propagation
+        Y -- "true" labels vector, same shape as a3
+        
+        Returns:
+        cost - value of the cost function
+        """
+        m = Y.shape[1]
+        
+        logprobs = np.multiply(-np.log(a3),Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+        cost = 1./m * np.sum(logprobs)
+        
+        return cost
+    
+    def update_parameters_with_gd(self, parameters, grads, learning_rate):
+        """
+        Update parameters using one step of gradient descent
+        
+        Arguments:
+        parameters -- python dictionary containing your parameters to be updated:
+                        parameters['W' + str(l)] = Wl
+                        parameters['b' + str(l)] = bl
+        grads -- python dictionary containing your gradients to update each parameters:
+                        grads['dW' + str(l)] = dWl
+                        grads['db' + str(l)] = dbl
+        learning_rate -- the learning rate, scalar.
+        
+        Returns:
+        parameters -- python dictionary containing your updated parameters 
+        """
+
+        L = len(parameters) // 2 # number of layers in the neural networks
+
+        # Update rule for each parameter
+        for l in range(L):
+            ### START CODE HERE ### (approx. 2 lines)
+            parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * grads["dW" + str(l+1)]
+            parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * grads["db" + str(l+1)]
+            ### END CODE HERE ###
+            
+        return parameters
+
+    def plot_decision_boundary(self, model, X, y):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+        y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+        h = 0.01
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        # Predict the function value for the whole grid
+        Z = model(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+        plt.ylabel('x2')
+        plt.xlabel('x1')
+        plt.scatter(X[0, :], X[1, :], c=y.flatten(), cmap=plt.cm.Spectral)
+        # plt.subplots(figsize=(5,4))
+        plt.show()
+        
+    def model(self, X, Y, layers_dims, optimizer, learning_rate = 0.0007, mini_batch_size = 64, beta = 0.9,
           beta1 = 0.9, beta2 = 0.999,  epsilon = 1e-8, num_epochs = 10000, print_cost = True):
         """
         3-layer neural network model which can be run in different optimizer modes.
@@ -3041,23 +3237,22 @@ class Coding2_2(CodingWorks):
         t = 0                            # initializing the counter required for Adam update
         seed = 10                        # For grading purposes, so that your "random" minibatches are the same as ours
         
-        # Initialize parameters
-        parameters = initialize_parameters(layers_dims)
+        # 构造各层W、b的默认值
+        parameters = self.initialize_parameters(layers_dims)
 
         # Initialize the optimizer
         if optimizer == "gd":
             pass # no initialization required for gradient descent
         elif optimizer == "momentum":
-            v = initialize_velocity(parameters)
+            v = self.initialize_velocity(parameters)
         elif optimizer == "adam":
-            v, s = initialize_adam(parameters)
+            v, s = self.initialize_adam(parameters)
         
         # Optimization loop
         for i in range(num_epochs):
-            
             # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
             seed = seed + 1
-            minibatches = random_mini_batches(X, Y, mini_batch_size, seed)
+            minibatches = self.random_mini_batches(X, Y, mini_batch_size, seed)
 
             for minibatch in minibatches:
 
@@ -3065,22 +3260,22 @@ class Coding2_2(CodingWorks):
                 (minibatch_X, minibatch_Y) = minibatch
 
                 # Forward propagation
-                a3, caches = forward_propagation(minibatch_X, parameters)
+                a3, caches = self.forward_propagation(minibatch_X, parameters)
 
                 # Compute cost
-                cost = compute_cost(a3, minibatch_Y)
+                cost = self.compute_cost(a3, minibatch_Y)
 
                 # Backward propagation
-                grads = backward_propagation(minibatch_X, minibatch_Y, caches)
+                grads = self.backward_propagation(minibatch_X, minibatch_Y, caches)
 
                 # Update parameters
                 if optimizer == "gd":
-                    parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+                    parameters = self.update_parameters_with_gd(parameters, grads, learning_rate)
                 elif optimizer == "momentum":
-                    parameters, v = update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
+                    parameters, v = self.update_parameters_with_momentum(parameters, grads, v, beta, learning_rate)
                 elif optimizer == "adam":
                     t = t + 1 # Adam counter
-                    parameters, v, s = update_parameters_with_adam(parameters, grads, v, s,
+                    parameters, v, s = self.update_parameters_with_adam(parameters, grads, v, s,
                                                                 t, learning_rate, beta1, beta2,  epsilon)
             
             # Print the cost every 1000 epoch
@@ -3097,6 +3292,122 @@ class Coding2_2(CodingWorks):
         plt.show()
 
         return parameters
+
+    def predict(self, X, y, parameters):
+        """
+        This function is used to predict the results of a  n-layer neural network.
+        
+        Arguments:
+        X -- data set of examples you would like to label
+        parameters -- parameters of the trained model
+        
+        Returns:
+        p -- predictions for the given dataset X
+        """
+        
+        m = X.shape[1]
+        p = np.zeros((1,m), dtype = np.int)
+        
+        # Forward propagation
+        a3, caches = self.forward_propagation(X, parameters)
+        
+        # convert probas to 0/1 predictions
+        for i in range(0, a3.shape[1]):
+            if a3[0,i] > 0.5:
+                p[0,i] = 1
+            else:
+                p[0,i] = 0
+
+        # print results
+
+        #print ("predictions: " + str(p[0,:]))
+        #print ("true labels: " + str(y[0,:]))
+        print("Accuracy: "  + str(np.mean((p[0,:] == y[0,:]))))
+        
+        return p
+
+    def predict_dec(self, parameters, X):
+        """
+        Used for plotting decision boundary.
+        
+        Arguments:
+        parameters -- python dictionary containing your parameters 
+        X -- input data of size (m, K)
+        
+        Returns
+        predictions -- vector of predictions of our model (red: 0 / blue: 1)
+        """
+        
+        # Predict using forward propagation and a classification threshold of 0.5
+        a3, cache = self.forward_propagation(X, parameters)
+        predictions = (a3 > 0.5)
+        return predictions
+
+    def tc6(self):
+        train_X, train_Y = self.load_dataset()
+        layers_dims = [train_X.shape[0], 5, 2, 1] # 节点个数：300 - 5 - 2 - 1
+        parameters = self.model(train_X, train_Y, layers_dims, optimizer = "gd") # 使用普通的梯度下降
+
+        # Predict
+        predictions = self.predict(train_X, train_Y, parameters)
+
+        # Plot decision boundary
+        plt.title("Model with Gradient Descent optimization")
+        axes = plt.gca()
+        axes.set_xlim([-1.5,2.5])
+        axes.set_ylim([-1,1.5])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+    def tc7(self):
+        train_X, train_Y = self.load_dataset()
+        layers_dims = [train_X.shape[0], 5, 2, 1]
+        parameters = self.model(train_X, train_Y, layers_dims, beta = 0.9, optimizer = "momentum")
+
+        # Predict
+        predictions = self.predict(train_X, train_Y, parameters)
+
+        # Plot decision boundary
+        plt.title("Model with Momentum optimization")
+        axes = plt.gca()
+        axes.set_xlim([-1.5,2.5])
+        axes.set_ylim([-1,1.5])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+    def tc8(self):
+        train_X, train_Y = self.load_dataset()
+        layers_dims = [train_X.shape[0], 5, 2, 1]
+        parameters = self.model(train_X, train_Y, layers_dims, optimizer = "adam")
+
+        # Predict
+        predictions = self.predict(train_X, train_Y, parameters)
+
+        # Plot decision boundary
+        plt.title("Model with Adam optimization")
+        axes = plt.gca()
+        axes.set_xlim([-1.5,2.5])
+        axes.set_ylim([-1,1.5])
+        self.plot_decision_boundary(lambda x: self.predict_dec(parameters, x.T), train_X, train_Y)
+
+class Coding2_3(CodingWorks):
+    def tc1(self):
+        n = 4
+        np.random.seed()
+        r = -1 * n * np.random.rand(n)
+        alpha = 10 ** r
+        for i in range(n):
+            logging.info('%.5f' % alpha[i])
+
+    def tc2(self):
+        np.random.seed()
+        r = 1 + 2 * np.random.rand(4)
+        logging.info('1-β：')
+        for i in range(4):
+            logging.info('%.5f' % (10 ** (-r[i])))
+        beta = 1 - 10 **(-r)
+        logging.info('β：')
+        for i in range(4):
+            logging.info('%.5f' % beta[i])
+        
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
