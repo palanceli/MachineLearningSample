5b27614ded3fb3889f5375239bef10b5fe50d149
palance <palanceli@163.com>
2018-05-09 11:31:42 -0700

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -4420,6 +4420,111 @@ class Coding4_1(CodingWorks):
 
 class Coding4_2_KerasTutorial(CodingWorks):
     ''' Keras Tutorial Happy House'''
+
+    def load_dataset(self):
+        trainDatasetPath = os.path.join(self.rootDir, 'coding4_2/KerasTutorial/datasets/train_happy.h5')
+        testDatasetPath = os.path.join(self.rootDir, 'coding4_2/KerasTutorial/datasets/test_happy.h5')
+
+        train_dataset = h5py.File(trainDatasetPath, "r")
+        train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+        train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+        test_dataset = h5py.File(testDatasetPath, "r")
+        test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+        test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+        classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+        
+        train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+        test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+        
+        return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+    def tc1(self):
+        ''' 输出训练集和测试集的个数和维度。训练集600条数据，测试集150条数据，均为64×64像素，3通道 '''
+        X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = self.load_dataset()
+
+        # Normalize image vectors
+        X_train = X_train_orig/255.
+        X_test = X_test_orig/255.
+
+        # Reshape
+        Y_train = Y_train_orig.T
+        Y_test = Y_test_orig.T
+
+        logging.info ("number of training examples = " + str(X_train.shape[0]))
+        logging.info ("number of test examples = " + str(X_test.shape[0]))
+        logging.info ("X_train shape: " + str(X_train.shape))
+        logging.info ("Y_train shape: " + str(Y_train.shape))
+        logging.info ("X_test shape: " + str(X_test.shape))
+        logging.info ("Y_test shape: " + str(Y_test.shape))
+
+    def model(self, input_shape):
+        # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
+        X_input = Input(input_shape)
+
+        # Zero-Padding: pads the border of X_input with zeroes
+        X = ZeroPadding2D((3, 3))(X_input)
+
+        # CONV -> BN -> RELU Block applied to X
+        X = Conv2D(32, (7, 7), strides = (1, 1), name = 'conv0')(X)
+        X = BatchNormalization(axis = 3, name = 'bn0')(X)
+        X = Activation('relu')(X)
+
+        # MAXPOOL
+        X = MaxPooling2D((2, 2), name='max_pool')(X)
+
+        # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+        X = Flatten()(X)
+        X = Dense(1, activation='sigmoid', name='fc')(X)
+
+        # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+        model = Model(inputs = X_input, outputs = X, name='HappyModel')
+
+        return model
+
+    def HappyModel(self, input_shape):
+        """
+        Implementation of the HappyModel.
+        
+        Arguments:
+        input_shape -- shape of the images of the dataset
+
+        Returns:
+        model -- a Model() instance in Keras
+        """
+        
+        ### START CODE HERE ###
+        # Feel free to use the suggested outline in the text above to get started, and run through the whole
+        
+        # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
+        X_input = Input(input_shape)
+
+        # Zero-Padding: pads the border of X_input with zeroes
+        X = ZeroPadding2D((3, 3))(X_input)
+
+        # CONV -> BN -> RELU Block applied to X
+        X = Conv2D(32, (7, 7), strides = (1, 1), name = 'conv0')(X)
+        X = BatchNormalization(axis = 3, name = 'bn0')(X)
+        X = Activation('relu')(X)
+
+        # MAXPOOL
+        X = MaxPooling2D((2, 2), name='max_pool')(X)
+
+        # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
+        X = Flatten()(X)
+        X = Dense(1, activation='sigmoid', name='fc')(X)
+
+        # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
+        model = Model(inputs = X_input, outputs = X, name='HappyModel')
+        # exercise (including the later portions of this notebook) once. The come back also try out other
+        # network architectures as well. 
+        
+        
+        ### END CODE HERE ###
+        
+        return model
+
     def tcMain(self):
         pass
 
