9bd11aee78b70744e900ae77bf1f1ad25707d410
palanceli <palanceli@163.com>
2018-05-15 21:41:31 +0800

    commit changes

--- DeepLearningAIHomeWorks/mywork.py
+++ DeepLearningAIHomeWorks/mywork.py
@@ -25,9 +25,9 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 
 from keras import layers
-from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
 from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.preprocessing import image
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
@@ -36,6 +36,8 @@ import pydot
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 from keras.utils import plot_model
+from keras.initializers import glorot_uniform
+import scipy.misc
 
 import keras.backend as K
 from matplotlib.pyplot import imshow
@@ -53,6 +55,7 @@ class CodingWorks(unittest.TestCase):
         plt.rcParams['image.cmap'] = 'gray'
 
         K.set_image_data_format('channels_last')
+        K.set_learning_phase(1)
 
     def showH5Group(self, groupObj):
         logging.info('group name:%s, shape:%s' % (groupObj.name, groupObj.shape))
@@ -4557,6 +4560,274 @@ class Coding4_2_KerasTutorial(CodingWorks):
         plot_model(happyModel, to_file='HappyModel.png')
         SVG(model_to_dot(happyModel).create(prog='dot', format='svg'))
 
+class Coding4_2_ResidualNetworks(CodingWorks):
+    def identity_block(self, X, f, filters, stage, block):
+        """
+        实现恒等块
+
+        Arguments:
+        X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+        f -- integer, specifying the shape of the middle CONV's window for the main path
+        filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+        stage -- integer, used to name the layers, depending on their position in the network
+        block -- string/character, used to name the layers, depending on their position in the network
+        
+        Returns:
+        X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+        """
+        
+        # defining name basis
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+        
+        # Retrieve Filters
+        F1, F2, F3 = filters
+        
+        # Save the input value. You'll need this later to add back to the main path. 
+        X_shortcut = X
+        
+        # 主路径上第一个组件：卷积 - BN - RELU
+        X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+        X = Activation('relu')(X)
+        
+        # 主路径上第二个组件：卷积 - BN - RELU
+        X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+        X = Activation('relu')(X)
+
+        # 主路径上第三个组件：卷积 - BN
+        X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c',kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+        # 将X_shortcut添加到主路径并传入RELU激活函数
+        X = Add()([X, X_shortcut])
+        X = Activation('relu')(X)
+        
+        
+        return X
+
+    def tc1(self):
+        ''' 验证恒等块 '''
+        tf.reset_default_graph()
+
+        with tf.Session() as test:
+            np.random.seed(1)
+            A_prev = tf.placeholder("float", [3, 4, 4, 6])
+            X = np.random.randn(3, 4, 4, 6)
+            A = self.identity_block(A_prev, f = 2, filters = [2, 4, 6], stage = 1, block = 'a')
+            test.run(tf.global_variables_initializer())
+            out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
+            logging.info ("out = " + str(out[0][1][1][0]))
+
+    def convolutional_block(self, X, f, filters, stage, block, s = 2):
+        """
+        实现卷积块
+
+        Arguments:
+        X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+        f -- integer, specifying the shape of the middle CONV's window for the main path
+        filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+        stage -- integer, used to name the layers, depending on their position in the network
+        block -- string/character, used to name the layers, depending on their position in the network
+        s -- Integer, specifying the stride to be used
+        
+        Returns:
+        X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+        """
+        
+        # defining name basis
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+        
+        # Retrieve Filters
+        F1, F2, F3 = filters
+        
+        # Save the input value
+        X_shortcut = X
+
+
+        ##### MAIN PATH #####
+        # First component of main path 
+        X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+        X = Activation('relu')(X)
+        
+        ### START CODE HERE ###
+
+        # Second component of main path (≈3 lines)
+        X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1, 1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+        X = Activation('relu')(X)
+
+        # Third component of main path (≈2 lines)
+        X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+        ##### SHORTCUT PATH #### (≈2 lines)
+        X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s, s), padding = 'valid', name = conv_name_base + '1', kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
+        X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+        # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+        X = Add()([X, X_shortcut])
+        X = Activation('relu')(X)
+        
+        ### END CODE HERE ###
+        
+        return X
+
+    def tc2(self):
+        ''' 验证卷积块 '''
+        tf.reset_default_graph()
+
+        with tf.Session() as test:
+            np.random.seed(1)
+            A_prev = tf.placeholder("float", [3, 4, 4, 6])
+            X = np.random.randn(3, 4, 4, 6)
+            A = self.convolutional_block(A_prev, f = 2, filters = [2, 4, 6], stage = 1, block = 'a')
+            test.run(tf.global_variables_initializer())
+            out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
+            print("out = " + str(out[0][1][1][0]))
+
+    def ResNet50(self, input_shape = (64, 64, 3), classes = 6):
+        """
+        Implementation of the popular ResNet50 the following architecture:
+        CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+        -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+        Arguments:
+        input_shape -- shape of the images of the dataset
+        classes -- integer, number of classes
+
+        Returns:
+        model -- a Model() instance in Keras
+        """
+        
+        # Define the input as a tensor with shape input_shape
+        X_input = Input(input_shape)
+
+        
+        # Zero-Padding
+        X = ZeroPadding2D((3, 3))(X_input)
+        
+        # Stage 1
+        X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+        X = Activation('relu')(X)
+        X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+        # Stage 2
+        X = self.convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+        X = self.identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+        X = self.identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+        ### START CODE HERE ###
+
+        # Stage 3 (≈4 lines)
+        X = self.convolutional_block(X, f = 3, filters = [128, 128, 512], stage = 3, block='a', s = 2)
+        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+        # Stage 4 (≈6 lines)
+        X = self.convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+        # Stage 5 (≈3 lines)
+        X = self.convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
+        X = self.identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+        X = self.identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+        # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+        X = AveragePooling2D((2, 2), strides=(2, 2))(X)
+        
+        ### END CODE HERE ###
+
+        # output layer
+        X = Flatten()(X)
+        X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+        
+        
+        # Create model
+        model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+        return model
+
+
+    def load_dataset(self):
+        trainDatasetPath = os.path.join(self.rootDir, 'coding4_2/ResNets/datasets/train_signs.h5')
+        testDatasetPath = os.path.join(self.rootDir, 'coding4_2/ResNets/datasets/test_signs.h5')
+
+        train_dataset = h5py.File(trainDatasetPath, "r")
+        train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+        train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+        test_dataset = h5py.File(testDatasetPath, "r")
+        test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+        test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+        classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+        
+        train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+        test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+        
+        return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+    def convert_to_one_hot(self, Y, C):
+        Y = np.eye(C)[Y.reshape(-1)].T
+        return Y
+
+    def tcMain(self):
+        X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = self.load_dataset()
+
+        # Normalize image vectors
+        X_train = X_train_orig/255.
+        X_test = X_test_orig/255.
+
+        # Convert training and test labels to one hot matrices
+        Y_train = self.convert_to_one_hot(Y_train_orig, 6).T
+        Y_test = self.convert_to_one_hot(Y_test_orig, 6).T
+
+        logging.info ("number of training examples = " + str(X_train.shape[0]))
+        logging.info ("number of test examples = " + str(X_test.shape[0]))
+        logging.info ("X_train shape: " + str(X_train.shape))
+        logging.info ("Y_train shape: " + str(Y_train.shape))
+        logging.info ("X_test shape: " + str(X_test.shape))
+        logging.info ("Y_test shape: " + str(Y_test.shape))
+
+        model = self.ResNet50(input_shape = (64, 64, 3), classes = 6)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.fit(X_train, Y_train, epochs = 2, batch_size = 32)
+        preds = model.evaluate(X_test, Y_test)
+        logging.info ("Loss = " + str(preds[0]))
+        logging.info ("Test Accuracy = " + str(preds[1]))
+
+        # 将模型转成可视化的图片
+        plot_model(model, to_file='model.png')
+        SVG(model_to_dot(model).create(prog='dot', format='svg'))
+
+    def tc3(self):
+        ''' 使用在GPU上训练出来的残差网络模型 '''
+        X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = self.load_dataset()
+
+        # Normalize image vectors
+        X_train = X_train_orig/255.
+        X_test = X_test_orig/255.
+
+        # Convert training and test labels to one hot matrices
+        Y_train = self.convert_to_one_hot(Y_train_orig, 6).T
+        Y_test = self.convert_to_one_hot(Y_test_orig, 6).T
+
+        modelPath = os.path.join(self.rootDir, 'coding4_2/ResNets/ResNet50.h5')
+        
+        model = load_model(modelPath) 
+        preds = model.evaluate(X_test, Y_test)
+        logging.info ("Loss = " + str(preds[0]))
+        logging.info ("Test Accuracy = " + str(preds[1]))
 
 if __name__ == '__main__':
     logFmt = '%(asctime)s %(lineno)04d %(levelname)-8s %(message)s'
